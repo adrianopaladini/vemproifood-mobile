591ff95a753a3a5162c030dfc6659d03c85371ad
Adriano Paladini <adrianopaladini@MacBookPro.local>
2018-11-21 20:40:23 -0200

    Add constants

---
+++ vemproiFood/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  vemproiFood
+//
+//  Created by Adriano Paladini on 21/11/18.
+//  Copyright © 2018 Adriano Paladini. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+    static let openWeatherAppId = "a67ef818d8c8e3945f7eee5f541c47e5"
+    static let openWeatherDomain = "http://api.openweathermap.org/data/2.5/"
+}
